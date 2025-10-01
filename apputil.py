14b4d0ab74a9cff51b08bf121330b3fa79df540a
Alex Toon <altoon@IU.EDU>
2025-10-01 18:25:38 -0400

    Init commit

--- apputil.py
+++ apputil.py
@@ -1,1 +1,21 @@
-# your code here ...
+# Exercise 1
+import os
+from apputil import Genius
+
+genius = Genius(access_token="access_token")
+
+
+
+
+# Exercise 2
+
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+search_term = "Missy Elliott"
+genius_search_url = f"http://api.genius.com/search?q={search_term}&access_token={ACCESS_TOKEN}"
+
+# here, we make a "GET" request to the Genius server
+response = requests.get(genius_search_url)
+json_data = response.json()
+
+
+#Exercise 3
